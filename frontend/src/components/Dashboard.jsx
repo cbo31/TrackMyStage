e27fa0763c710c53627dc99f3c12ba2b4e98125d
clementbo <clement.bourree.pro@gmail.com>
2026-03-16 10:56:11 +0100

    Add: 'Details' button and color to status

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,17 +1,46 @@
-import { Box, Button, Card, CardContent, Divider, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
+import { Box, Button, Card, Chip, CardContent, Divider, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
 import data from '/src/data.js'
 
 function Dashboard() {
 
+  const statusConfig = {
+    // TODO : modify chip's color
+    SENT: {label: "envoyée", color: "info"},
+    INTERVIEW: {label: "Entretien", color: "warning"},
+    REJECTED: {label: "Refusée", color: "error"},
+    TO_APPLY: {label: "A postuler", color: "default"},
+    NO_RESPONSE: {label: "Sans réponse", color: "secondary"},
+  }
+
   const columns = [
-    {label:"entreprise", key:"company"},
-    {label: "poste", key:"position"},
-    {label: "contact", key:"contact"},
-    {label: "date", key:"nextActionDate"},
-    {label: "etat", key:"status"},
-    {label: "ville", key:"town"},
+    {label:"Entreprise", key:"company"},
+    {label: "Poste", key:"position"},
+    {label: "Contact", key:"contact"},
+    {label: "Date", key:"nextActionDate"},
+    {
+      label: "Etat",
+      key: "status",
+      render: (row) => (
+        <Chip
+          label={statusConfig[row.status].label}
+          color={statusConfig[row.status].color}
+          sx={{
+            textTransform: 'uppercase',
+          }}
+        />
+      )
+    },
+    {label: "Ville", key:"town"},
+    { // add button beside backend data inside Table
+      label: "Actions",
+      key: "actions",
+      hideHeader: true, // enable hide row "actions" in th
+      render: (row) => (
+        <Button variant="contained" size="small"> details</Button>
+      )
+    }
   ];
-  
+
   return (
     <Box sx={{ width: 1000 }}>
       <Typography variant="h3" sx={{ color: "text.white" }}>Track My Stage</Typography>
@@ -33,9 +62,11 @@ function Dashboard() {
               <TableHead>
                 <TableRow>
                   {columns.map((col) => (
+                    !col.hideHeader && (
                     <TableCell key={col.key} align="center" sx={{ fontSize: 18, textTransform: 'uppercase', fontWeight: 600 }}>
                       {col.label}
                     </TableCell>
+                  )
                   ))}
                 </TableRow>
               </TableHead>
@@ -44,7 +75,7 @@ function Dashboard() {
                   <TableRow key={d.id}>
                     {columns.map((col) => (
                       <TableCell key={col.key} align="center" sx={{ fontSize: 16 }}>
-                        {d[col.key]}
+                        {col.render ? col.render(d) : d[col.key]}
                       </TableCell>
                     ))}
                   </TableRow>
