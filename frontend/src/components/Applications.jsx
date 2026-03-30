3da5ec228d6c35284705bff1ecddd1c6ec2c52fd
clementbo <clement.bourree.pro@gmail.com>
2026-03-30 19:17:40 +0200

    Add: Custom AppBar with 'TrackMyStage' and 'disconnect' button. Fix: file's name for Applications/Dashboard

---
+++ frontend/src/components/Applications.jsx
@@ -0,0 +1,109 @@
+import { useState } from 'react';
+import { Box, Button, Card, Chip, CardContent, Divider, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
+import NewApplication from '/src/components/NewApplication.jsx';
+import data from '/src/data.js'
+
+function Dashboard() {
+  const [open, setOpen] = useState(false);
+
+  // function to open/close dialog 'nouvelle candidature' throught properties
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const statusConfig = {
+    SENT: {label: "envoyée", color: "warning"},
+    INTERVIEW: {label: "Entretien", color: "success"},
+    REJECTED: {label: "Refusée", color: "error"},
+    TO_APPLY: {label: "A postuler", color: "default"},
+    NO_RESPONSE: {label: "Sans réponse", color: "secondary"},
+  }
+
+  const columns = [
+    {label:"Entreprise", key:"company"},
+    {label: "Poste", key:"position"},
+    {label: "Contact", key:"contact"},
+    {label: "Date", key:"nextActionDate"},
+    {
+      label: "Etat",
+      key: "status",
+      render: (row) => (
+        <Chip
+          label={statusConfig[row.status].label}
+          color={statusConfig[row.status].color}
+          sx={{
+            textTransform: 'uppercase',
+          }}
+        />
+      )
+    },
+    {label: "Ville", key:"town"},
+    { // add button beside backend data inside Table
+      label: "Actions",
+      key: "actions",
+      hideHeader: true, // enable hide row "actions" in th
+      render: (row) => (
+        <Button variant="contained" size="small"> details</Button>
+      )
+    }
+  ];
+
+  return (
+    <Box sx={{ width: 1000 }}>
+      <Box sx= {{
+        display: 'flex',
+        justifyContent: 'space-between'
+      }}>
+        <Typography variant="h3" sx={{ color: "text.white" }}>Vos Candidatures</Typography>
+        <Button variant="contained" onClick={handleOpen} sx={{ alignSelf: 'center' }}>nouvelle candidature</Button>
+
+        <NewApplication open={open} onClose={handleClose} />
+      </Box>
+  
+      <Divider sx={{ m: 2 }}/>
+
+      <Card
+        sx={{ 
+          display: "flex", 
+          alignItems: "center", 
+          flexDirection: "column", 
+          boxShadow: 16, 
+          bgcolor: "#F9FAFB",  
+          justifyContent: "center",
+        }}
+      >
+        <CardContent>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  {columns.map((col) => (
+                    !col.hideHeader && (
+                    <TableCell key={col.key} align="center" sx={{ fontSize: 18, textTransform: 'uppercase', fontWeight: 600 }}>
+                      {col.label}
+                    </TableCell>
+                  )
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.map((d) =>(
+                  <TableRow key={d.id}>
+                    {columns.map((col) => (
+                      <TableCell key={col.key} align="center" sx={{ fontSize: 16 }}>
+                        {col.render ? col.render(d) : d[col.key]}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </CardContent>
+      </Card>
+
+
+    </Box>
+  )
+};
+
+export default Dashboard
--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,109 +1,33 @@
-import { useState } from 'react';
-import { Box, Button, Card, Chip, CardContent, Divider, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
-import NewApplication from '/src/components/NewApplication.jsx';
-import data from '/src/data.js'
+import { Box, AppBar, Toolbar, Button, Typography, Paper } from '@mui/material'
+import Applications from '/src/components/Applications.jsx';
+
 
 function Dashboard() {
-  const [open, setOpen] = useState(false);
-
-  // function to open/close dialog 'nouvelle candidature' throught properties
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  const statusConfig = {
-    SENT: {label: "envoyée", color: "warning"},
-    INTERVIEW: {label: "Entretien", color: "success"},
-    REJECTED: {label: "Refusée", color: "error"},
-    TO_APPLY: {label: "A postuler", color: "default"},
-    NO_RESPONSE: {label: "Sans réponse", color: "secondary"},
-  }
-
-  const columns = [
-    {label:"Entreprise", key:"company"},
-    {label: "Poste", key:"position"},
-    {label: "Contact", key:"contact"},
-    {label: "Date", key:"nextActionDate"},
-    {
-      label: "Etat",
-      key: "status",
-      render: (row) => (
-        <Chip
-          label={statusConfig[row.status].label}
-          color={statusConfig[row.status].color}
-          sx={{
-            textTransform: 'uppercase',
-          }}
-        />
-      )
-    },
-    {label: "Ville", key:"town"},
-    { // add button beside backend data inside Table
-      label: "Actions",
-      key: "actions",
-      hideHeader: true, // enable hide row "actions" in th
-      render: (row) => (
-        <Button variant="contained" size="small"> details</Button>
-      )
-    }
-  ];
-
   return (
-    <Box sx={{ width: 1000 }}>
-      <Box sx= {{
-        display: 'flex',
-        justifyContent: 'space-between'
-      }}>
-        <Typography variant="h3" sx={{ color: "text.white" }}>Vos Candidatures</Typography>
-        <Button variant="contained" onClick={handleOpen} sx={{ alignSelf: 'center' }}>nouvelle candidature</Button>
-
-        <NewApplication open={open} onClose={handleClose} />
-      </Box>
-  
-      <Divider sx={{ m: 2 }}/>
-
-      <Card
-        sx={{ 
-          display: "flex", 
-          alignItems: "center", 
-          flexDirection: "column", 
-          boxShadow: 16, 
-          bgcolor: "#F9FAFB",  
-          justifyContent: "center",
-        }}
-      >
-        <CardContent>
-          <TableContainer>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  {columns.map((col) => (
-                    !col.hideHeader && (
-                    <TableCell key={col.key} align="center" sx={{ fontSize: 18, textTransform: 'uppercase', fontWeight: 600 }}>
-                      {col.label}
-                    </TableCell>
-                  )
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {data.map((d) =>(
-                  <TableRow key={d.id}>
-                    {columns.map((col) => (
-                      <TableCell key={col.key} align="center" sx={{ fontSize: 16 }}>
-                        {col.render ? col.render(d) : d[col.key]}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </CardContent>
-      </Card>
-
-
+    <>
+      <Box
+      sx={{
+        width: "100%",
+        px: 3,                   // padding horizontal (3 * 8px = 24px)
+        py: 2,                   // padding vertical
+        boxShadow: "0px 3px 12px rgba(0,0,0,0.3)",
+        borderRadius: 1,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        mb: 5
+      }}
+    >
+      <Typography variant="h5" sx={{ color: "white", fontWeight: "bold" }}>
+        TrackMyStage
+      </Typography>
+      <Button variant='contained'>déconnexion</Button>
     </Box>
+
+    <Applications />
+    </>
   )
+  
 };
 
 export default Dashboard
