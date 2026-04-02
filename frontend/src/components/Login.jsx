9ff37929e9f758124a58d85d3f78b1a8329b1eca
clementbo <clement.bourree.pro@gmail.com>
2026-04-02 16:21:08 +0200

    Fix: clean Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -8,17 +8,10 @@ function Login({ onLoginSuccess }) {
   const [message, setMessage] = useState('');
   const navigate = useNavigate();
 
-  console.log('🔴 Login.jsx est chargé !');
-  console.log('onLoginSuccess est:', onLoginSuccess);
-
-
   const handleLogin = async (e) => {
-    console.log('🟢 handleLogin called !');
     e.preventDefault(); // disable refresh page by wrong submit
 
     try {
-      console.log('Tentative de connexion...', {email, password});
-
       const res = await fetch('/api/login/', {
         method: 'POST',
         headers: {
@@ -27,20 +20,14 @@ function Login({ onLoginSuccess }) {
         body: JSON.stringify({ email, password }),
       });
 
-      console.log('Réponse reçue :', {statue: res.status, ok: res.ok});
-
       const data = await res.json();
-      console.log('Données parsées :', data);
 
       if(res.ok) {
-        console.log('✅ Connexion réussie');
         onLoginSuccess(data.user, data.access);
       } else {
-        console.log('❌ Erreur API:', data.error);
         setMessage(data.error);
       }
     } catch (error) { // error message if fetch does not work
-      console.error('💥 Exception:', error);
       setMessage('probleme de connexion au serveur');
     };
   };
