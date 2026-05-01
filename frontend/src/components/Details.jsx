9ee50e62193693c92d125e348a58165a13e073b4
clementbo <clement.bourree.pro@gmail.com>
2026-05-01 18:15:46 +0200

    Update: Details view design cleaned and more readable

--- frontend/src/components/Details.jsx
+++ frontend/src/components/Details.jsx
@@ -1,4 +1,4 @@
-import { Dialog, Typography, Button, DialogTitle, DialogActions, DialogContent, TextField, IconButton, Stack, Box } from "@mui/material";
+import { Dialog, Typography, Button, DialogTitle, DialogActions, DialogContent, TextField, IconButton, Stack, Box, Divider } from "@mui/material";
 import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
 import CloseIcon from '@mui/icons-material/Close';
@@ -65,62 +65,75 @@ function Details({open, onClose, onSuccess, application}) {
   }
 
   return (
-    <Dialog open={open} onClose={onClose} >
+    <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth >
+
       <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", pr: 1}}>
-        <DialogTitle sx={{ textTransform: "uppercase", m:0, p:2 }}>details</DialogTitle>
+        <DialogTitle sx={{ m:0, p:2 }}>Détails pour "{application.position.toUpperCase()}" chez "{application.company.toUpperCase()}"</DialogTitle>
         <IconButton aria-label="close" onClick={handleClose}>
           <CloseIcon />
         </IconButton>
       </Box>
-
       <DialogContent>
+
         <Stack spacing={2}>
-          <Stack direction="row" spacing={2}>
-            <TextField type="text" name="company" label="Entreprise" size="small"
-              value={formData.company} onChange={handleChange} 
-            />
-            <TextField type="text" name="city" label="Ville" size="small" 
-              value={formData.city} onChange={handleChange} 
-            />
-          </Stack>
-          
           <TextField type="text" name="position" label="Poste" size="small" 
-            value={formData.position} onChange={handleChange} 
+            value={formData.position} onChange={handleChange}
+            slotProps={{
+              input: { sx: {fontSize: 20 }}
+            }}
           />
 
-          <TextField type="text" name="contact" label="Contact" size="small" 
-            value={formData.contact} onChange={handleChange} 
-          />
+          <Divider />
 
-          <Stack direction="row" spacing={2} alignItems="center">
-            <TextField type="text" name="date" label="Date" size="small" 
-              value={formData.date} onChange={handleChange} 
+          <Box sx={{ display: "grid", gridTemplateColumns: "1fr auto 1fr", gap: 2 }}>
+
+            <Stack spacing={2}>
+              <TextField type="text" name="company" label="Entreprise" size="small"
+                value={formData.company} onChange={handleChange} 
+              />
+              <TextField type="text" name="city" label="Ville" size="small" 
+                value={formData.city} onChange={handleChange} 
+              />
+              <TextField type="text" name="contact" label="Contact" size="small" 
+              value={formData.contact} onChange={handleChange} 
             />
-          
-            <FormControl fullWidth size="small">
-              <InputLabel id="status">Status</InputLabel>
-              <Select
-                labelId="status"
-                id="status-select"
-                label="Status"
-                name="status"
-                value={formData.status}
-                onChange={handleChange}
-              >
-                <MenuItem value={`sent`}>Envoyée</MenuItem>
-                <MenuItem value={`to_apply`}>A envoyer</MenuItem>
-                <MenuItem value={`no_response`}>Sans réponse</MenuItem>
-                <MenuItem value={`interview`}>Entretien</MenuItem>
-                <MenuItem value={`rejected`}>Refusé</MenuItem>
-              </Select>
-            </FormControl>
-          </Stack>
+            </Stack>
+
+            <Divider orientation="vertical" flexItem />
+
+            <Stack spacing={2}>
+              <TextField type="text" name="date" label="Date" size="small" 
+                value={formData.date} onChange={handleChange} 
+              />
+              <FormControl fullWidth size="small">
+                <InputLabel id="status">Status</InputLabel>
+                <Select
+                  labelId="status"
+                  id="status-select"
+                  label="Status"
+                  name="status"
+                  value={formData.status}
+                  onChange={handleChange}
+                >
+                  <MenuItem value={`sent`}>Envoyée</MenuItem>
+                  <MenuItem value={`to_apply`}>A envoyer</MenuItem>
+                  <MenuItem value={`no_response`}>Sans réponse</MenuItem>
+                  <MenuItem value={`interview`}>Entretien</MenuItem>
+                  <MenuItem value={`rejected`}>Refusé</MenuItem>
+                </Select>
+              </FormControl>
+            </Stack>
+
+          </Box>
+
+          <Divider />
 
           <TextField type="text" label="Notes" name="note" multiline rows={4} fullWidth
             value={formData.note} onChange={handleChange} 
           />
         </Stack>
       </DialogContent>
+
       <DialogActions>
         <Button onClick={handleDelete}>supprimer</Button>
         <Button onClick={handleSubmit}>modifier</Button>
